a120330a32b7de1cb3c49854d90bcbe8f20e929e
neeraja <neeraja_paladugu@outlook.com>
2020-11-04 16:15:23 -0500

    message

--- src/main/java/com/wozU/Deployment/DeploymentApplication.java
+++ src/main/java/com/wozU/Deployment/DeploymentApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeploymentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeploymentApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentApplication.class,args);
+    }
 }
