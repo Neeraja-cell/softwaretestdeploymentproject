5e27819982175c85483a64549963301be0810404
neeraja <neeraja_paladugu@outlook.com>
2020-11-04 15:40:23 -0500

    Html

--- src/main/java/com/wozU/Deployment/HelloController.java
+++ src/main/java/com/wozU/Deployment/HelloController.java
@@ -1,12 +1,14 @@
 package com.wozU.Deployment;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/hello")
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/get")
     public String getHello(){
-        return "Hello";
+        return "Hello there";
     }
 }
